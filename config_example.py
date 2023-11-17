4f844d6fd7703941d42ef78afeb6dfe4b8a7744e
Rubiks37 <jasxy4321@gmail.com>
2023-11-16 23:31:23 -0500

    change GAMES to be a dict mapping id to name

--- config_example.py
+++ config_example.py
@@ -2,5 +2,5 @@
 HEADER = {"User-Agent": "SpeedrunBot for Discord by rubiks37 (Contact on Discord)"}
 
 TOKEN = 'YOUR_BOT_TOKEN'
-GAMES = ['YOUR', 'GAME', 'IDs', 'HERE']
+GAMES = {'GAME1_id': 'GAME1_name', 'GAME2_id': 'GAME2_name'}  # and so on
 
